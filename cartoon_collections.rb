b0d8abf21df6813f0d82ad010e57784f817260a6
Taylor Gonzalez <taylorgon@gmail.com>
2020-03-17 17:07:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   if array.include?(cheese_types)
-   array(cheese_types).detect
+   array(cheese_types)
   else
     nil
   end 
